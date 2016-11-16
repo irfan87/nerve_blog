6693b506d640ed078d65abffbc468ee4608b8699
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2016-11-16 23:18:12 +0800

    add post and comment model

---
+++ db/migrate/20161116151442_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :post_title
+      t.string :post_body
+
+      t.timestamps
+    end
+  end
+end
