53e07a0f93a8ffb3ae88557b04437c4e6e396814
Ahmad Irfan Mohammad Shukri <nerve2009@yahoo.com>
2016-11-16 23:28:48 +0800

    first model test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+	validates :post_title, presence: true
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should not save post title without title" do
+		post = Post.new
+		assert_not post.save
+	end
 end
